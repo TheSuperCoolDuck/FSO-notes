a4d18882a53993b4c82e936dcf87674fa23a3591
Daniel Chen <dchen0058@gmail.com>
2021-12-13 20:13:28 +1100

    converted to mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Note = require('./models/note')
 const { response, request } = require('express')
 const express = require('express')
 const cors = require('cors')
@@ -7,44 +9,20 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-]
-
 app.get('/',(request,response)=>{
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes',(request,response)=>{
-    response.json(notes)
+    Note.find({}).then(notes=>{
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request,response)=>{
-    const id = request.params.id
-    const note = notes.find(note=>note.id==id)
-    if(note){
+    Note.findById(request.params.id).then(note=>{
         response.json(note)
-    } else{
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response)=>{
@@ -64,25 +42,24 @@ const generateId=()=>{
 app.post('/api/notes',(request,response)=>{
     const body=request.body
     
-    if(!body.content){
+    if(body.content==undefined){
         return response.status(400).json({
             error:'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote=>{
+        response.json(savedNote)
+    })
 })
 
-const PORT = process.env.PORT||3001
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server running port ${PORT}`)
 })
