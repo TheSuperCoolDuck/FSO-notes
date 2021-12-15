94a476f175f78fda8b3066d180c73873f788cdc1
Daniel Chen <dchen0058@gmail.com>
2021-12-15 19:32:10 +1100

    added vaildation for the length of content

--- index.js
+++ index.js
@@ -9,14 +9,8 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
-app.post('/api/notes',(request,response)=>{
+app.post('/api/notes',(request,response,next)=>{
     const body=request.body
-    
-    if(body.content==undefined){
-        return response.status(400).json({
-            error:'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
@@ -24,9 +18,12 @@ app.post('/api/notes',(request,response)=>{
         date: new Date(),
     })
 
-        note.save().then(savedNote=>{
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote=>savedNote.toJSON())
+        .then(savedAndFormattedNote=>{
+            response.json(savedAndFormattedNote)
+        })
+        .catch(error=>next(error))
 })
 
 app.get('/',(request,response)=>{
@@ -85,6 +82,8 @@ const errorHandler=(error, request, response, next) =>{
 
     if(error.name === "CastError"){
         return response.status(400).send({error:'malformatted id'})
+    } else if(error.name==="ValidationError"){
+        return response.status(400).send({error:error.message})
     }
 
     next(error)
